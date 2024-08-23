09ed6bd561841b6b87896b38404a8f79fc91baaf
RafiaSuleman <rafiasuleman47@gmail.com>
2024-08-23 14:54:06 +0500

    commit -8

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -44,7 +44,7 @@ const Contact = () => {
       className="py-6 "
     >
       <div className="container mx-auto">
-          <div className="flex flex-col xl:flex-row gap-[30px] bg-purple-300 ">
+          <div className="flex flex-col xl:flex-row gap-[30px] ">
             <div className="xl:w-[54%] order-2 xl:order-none">
               <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl ">
                   <h3 className="text-4xl text-accent">Let us work Together</h3>
@@ -77,7 +77,7 @@ const Contact = () => {
                </form>
             </div>
              
-          </div> 
+          
           {/* info */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
      
@@ -99,6 +99,7 @@ const Contact = () => {
               })}
             </ul> 
           </div>
+          </div> 
       </div>
     </motion.section>
   );
