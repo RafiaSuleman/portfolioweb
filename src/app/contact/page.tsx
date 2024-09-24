a812e5955c824b9ef21e5739607ac6ff643d1fd6
RafiaSuleman <rafiasuleman47@gmail.com>
2024-09-24 09:08:20 +0500

    contactpage fixed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -48,7 +48,7 @@ const Contact = () => {
             <div className="xl:w-[54%] order-2 xl:order-none">
               <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl ">
                   <h3 className="text-4xl text-accent">Let us Connect</h3>
-                  <p className="text-white/60">Whether you have a project in mind, need assistance, feel free to reach out. I'd love to hear from you!</p> 
+                  <p className="text-white/60">Whether you have a project in mind, need assistance, feel free to reach out. I did love to hear from you!</p> 
                  <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                   <Input type='firstname' placeholder="Firstname"/>
 
